15898ef161996a35a885b6d8b44f4c02c700a78d
will <wlymes@gmail.com>
2024-07-30 11:39:54 -0300

    feat: creating user tester hook

---
+++ src/hooks/use-user-tester.ts
@@ -0,0 +1,21 @@
+import { USER_TESTER } from '@/static/env'
+import { useAuth } from './use-auth'
+import { useState, useEffect } from 'react'
+
+const userTester = USER_TESTER
+
+export const useUserTester = () => {
+  const [isUserTester, setIsUserTester] = useState(false)
+
+  const { user } = useAuth()
+
+  useEffect(() => {
+    if (user?.email === userTester) {
+      setIsUserTester(true)
+    }
+  }, [user])
+
+  return {
+    isUserTester,
+  }
+}
--- src/static/env.ts
+++ src/static/env.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? ''
+export const USER_TESTER = process.env.NEXT_PUBLIC_USER_TESTER ?? ''

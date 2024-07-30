5a5f2739cfd67665a09ce5a61897fe9d32e8ad67
will <wlymes@gmail.com>
2024-07-30 10:50:25 -0300

    feat: show and hide password function

--- src/components/features/auth/_components/signin-form.tsx
+++ src/components/features/auth/_components/signin-form.tsx
@@ -18,11 +18,14 @@ import { SigninDto, signinSchema } from '@/schemas/signin-schema'
 import { signin } from '../actions/signin-action.service'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
-import { LoaderCircle } from 'lucide-react'
+import { Eye, EyeOff, LoaderCircle } from 'lucide-react'
+import { useToggle } from '@/hooks/use-toggle'
 
 export const SigninForm = () => {
   const [isLoading, setIsLoading] = useState(false)
   const router = useRouter()
+
+  const { isOpen, toggleModal } = useToggle()
 
   const form = useForm<SigninDto>({
     resolver: zodResolver(signinSchema),
@@ -79,11 +82,24 @@ export const SigninForm = () => {
             <FormItem className="w-full">
               <FormLabel>Senha</FormLabel>
               <FormControl>
-                <Input
-                  placeholder="Digite sua senha"
-                  type="password"
-                  {...field}
-                />
+                <div className="relative flex">
+                  <Input
+                    placeholder="Digite sua senha"
+                    type={isOpen ? 'text' : 'password'}
+                    {...field}
+                  />
+                  {isOpen ? (
+                    <Eye
+                      onClick={toggleModal}
+                      className="absolute right-4 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-muted-foreground"
+                    />
+                  ) : (
+                    <EyeOff
+                      onClick={toggleModal}
+                      className="absolute right-4 top-1/2 size-5 -translate-y-1/2 cursor-pointer text-muted-foreground"
+                    />
+                  )}
+                </div>
               </FormControl>
               <div className="h-5">
                 <FormMessage />
